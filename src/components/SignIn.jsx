b071808235bb0c3220ed1a8f11103bfd6f99c161
saadshakeel123 <saad03112503171@gmail.com>
2023-04-02 14:41:40 +0500

    set ho gia

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,17 +2,29 @@ import React from 'react'
 import GoogleButton from 'react-google-button'
 
 import { auth } from "../firebase"
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth"
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
+// const googleSignIn = () => {
+//   const provider = new GoogleAuthProvider();
+//   signInWithRedirect(auth, provider);
+// }
 const googleSignIn = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithRedirect(auth, provider);
-}
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+    // signInWithRedirect(auth, provider)
+  };
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 function SignIn() {
   return (
     <div className='flex justify-center py-14 rounded-lg'>
-      <GoogleButton onClick={googleSignIn} />
+      <GoogleButton onClick={handleGoogleSignIn} />
     </div>
   )
 }
